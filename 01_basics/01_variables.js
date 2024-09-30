64add002c67738a9d62a549046f04b24308ccafe
pshukla1234 <102023172+pshukla1234@users.noreply.github.com>
2024-09-30 05:39:39 +0000

    new varibales file notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "pshuklaplanit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rewa"
+
+// accountId = 123456 const values can't be changed 
+
+/*
+prefer not to use var , because of issue scope and block scope
+*/
+
+accountEmail = "pranjal372020@gmail.com"
+accountPassword = "212121"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,26 @@
+/*
+use strict function was previously mention to follow strict protocol to use JS
+code redebility should be prioritised first
+*/
+
+// data types
+// number => 2 to power 53 or 54
+// string =>" could be written in double quote or single quote but should be written in double quote is preferred"
+// bigInt for bigger number 
+// boolean => true or false
+// null => standalone
+// undefined
+// symbol = to define individual components or unique
+// these above all are primitive data types in javascript
+// object will be discussing through out the course 
+
+let name = "Pranjl Shukla"
+let phoneNumber = 77701234590
+let isAvailable = false
+let whenAvailable = null
+let newSeries ; 
+
+// typeOf function is used to find out the type of data type like example the given thing could be string , number , boolean , or any thing
+
+console.log(typeof name);
+
