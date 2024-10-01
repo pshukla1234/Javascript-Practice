b9a2d7c497005907bee24d1acb2bbe6d567466f8
pshukla1234 <102023172+pshukla1234@users.noreply.github.com>
2024-10-01 03:59:27 +0000

    conversions

---
+++ 01_basics/02_conversions.js
@@ -0,0 +1,26 @@
+let score = 33
+
+console.log(typeof score)
+
+let number =1
+
+let boolnum = Boolean(number)
+console.log(boolnum)
+
+
+let number1 =0
+
+let boolnum1= Boolean(number1)
+console.log(boolnum1)
+
+
+let number2 =""
+
+let boolnum2 = Boolean(number2)
+console.log(boolnum2)
+
+
+let string3 ="33abc"
+let num1 = Number(string3)
+console.log(num1)
+
