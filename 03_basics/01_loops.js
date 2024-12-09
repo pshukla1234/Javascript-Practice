6c456b8beb539a205919441ee96a08f01a6c1943
pshukla1234 <102023172+pshukla1234@users.noreply.github.com>
2024-12-09 10:56:46 +0000

    new files

---
+++ 03_basics/01_loops.js
@@ -0,0 +1,24 @@
+// for loop
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     if (i==5) {
+//         console.log("5 is the best number");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+// Table from 1-20 to understand inner loop and outer loop
+
+for (let i = 1; i <=20; i++) {
+    console.log(`Table of ${i}`)
+
+    for (let j = 1; j <=10; j++) {
+        console.log(i + ' * '+ j + ' = '+ i*j);
+        
+    }
+
+
+    
+}
---
+++ 03_basics/01_switch.js
@@ -0,0 +1,15 @@
+// understanding the switch and break 
+
+switch (10) {
+    case 3:
+        console.log("number is 3"); 
+        break;
+
+        case 1:
+        console.log("number is 1"); 
+        break;
+
+    default:
+        console.log(" default is 0")
+        break;
+}
